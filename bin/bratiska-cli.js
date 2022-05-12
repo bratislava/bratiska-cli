1eca69f2b22c5bb85a0c05c56323e9919a1e14ed
Richi <richard.dvorsky@onlim.com>
2022-05-12 15:13:33 +0200

    more updates

--- bin/bratiska-cli.js
+++ bin/bratiska-cli.js
@@ -1,13 +1,39 @@
 #!/usr/bin/env node
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const chalk_1 = require("chalk");
+const chalk_1 = __importDefault(require("chalk"));
 //import clear from 'clear';
-const figlet = require("figlet");
+const figlet = __importStar(require("figlet"));
 const commander_1 = require("commander");
-const cp = require("child_process");
+const cp = __importStar(require("child_process"));
 const child_process_1 = require("child_process");
-const fs = require("fs");
+const fs = __importStar(require("fs"));
 const log = console.log.bind(console);
 function image_tag(options) {
     return `${image(options)}:${tag(options)}`;
