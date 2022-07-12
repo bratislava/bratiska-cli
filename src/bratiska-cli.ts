1411f3e83614c8bb9730895d25b7dbe2bc842d9a
Richi <richard.dvorsky@bratislava.sk>
2022-07-12 18:05:53 +0200

    added tags detection

--- src/bratiska-cli.ts
+++ src/bratiska-cli.ts
@@ -7,7 +7,7 @@ import { program } from 'commander';
 import { Steps } from './steps';
 import * as helpers from './helpers';
 
-const version = '1.5.0';
+const version = '1.5.1';
 const steps = new Steps();
 
 try {
