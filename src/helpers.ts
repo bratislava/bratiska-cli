4096b69f3c188f257bd8653d065bafdb36639f0a
Richi <richard.dvorsky@bratislava.sk>
2022-08-08 11:47:33 +0200

    added env

--- src/helpers.ts
+++ src/helpers.ts
@@ -234,41 +234,73 @@ export function assign_env_vars(options: any) {
   if (image_tag(options) === '//') {
     throw new Error('Image have to be filled! Please use --image <image_tag>.');
   }
+
   if (!process.env['BUILD_REPOSITORY_URI']) {
     process.env['BUILD_REPOSITORY_URI'] = options.repository_uri;
+    print_if_debug(
+      options,
+      `'BUILD_REPOSITORY_URI'=${process.env['BUILD_REPOSITORY_URI']}`,
+    );
   }
   if (!process.env['BUILD_REPOSITORY_NAME']) {
     process.env['BUILD_REPOSITORY_NAME'] = options.deployment;
+    print_if_debug(
+      options,
+      `'BUILD_REPOSITORY_NAME'=${process.env['BUILD_REPOSITORY_NAME']}`,
+    );
   }
   if (!process.env['DEPLOYMENT_ENV']) {
     process.env['DEPLOYMENT_ENV'] = options.deployment_env;
+    print_if_debug(
+      options,
+      `'DEPLOYMENT_ENV'=${process.env['DEPLOYMENT_ENV']}`,
+    );
+  }
+  if (!process.env['ENV']) {
+    process.env['ENV'] = options.env;
+    print_if_debug(options, `'ENV'=${process.env['ENV']}`);
   }
   if (!process.env['HOSTNAME']) {
     process.env['HOSTNAME'] = options.host;
+    print_if_debug(options, `'HOSTNAME'=${process.env['HOSTNAME']}`);
   }
   if (!process.env['IMAGE_TAG']) {
     process.env['IMAGE_TAG'] = image_tag(options);
+    print_if_debug(options, `'IMAGE_TAG'=${process.env['IMAGE_TAG']}`);
   }
   if (!process.env['IMAGE']) {
     process.env['IMAGE'] = image(options);
+    print_if_debug(options, `'IMAGE'=${process.env['IMAGE']}`);
   }
   if (!process.env['TAG']) {
     process.env['TAG'] = tag(options);
-  }
-  if (!process.env['COMMIT']) {
-    process.env['COMMIT'] = options.commit;
-  }
-  if (!process.env['NAMESPACE']) {
-    process.env['NAMESPACE'] = options.namespace;
-  }
-  if (!process.env['IMAGE_PULL_SECRET']) {
-    process.env['IMAGE_PULL_SECRET'] = pull_secret_name(options);
-  }
-  if (!process.env['INTERNAL_APP_PORT']) {
-    process.env['INTERNAL_APP_PORT'] = options.app_port;
+    print_if_debug(options, `'TAG'=${process.env['TAG']}`);
   }
   if (!process.env['GIT_TAG']) {
     process.env['GIT_TAG'] = options.gittag;
+    print_if_debug(options, `'GIT_TAG'=${process.env['GIT_TAG']}`);
+  }
+  if (!process.env['COMMIT']) {
+    process.env['COMMIT'] = options.commit;
+    print_if_debug(options, `'COMMIT'=${process.env['COMMIT']}`);
+  }
+  if (!process.env['NAMESPACE']) {
+    process.env['NAMESPACE'] = options.namespace;
+    print_if_debug(options, `'NAMESPACE'=${process.env['NAMESPACE']}`);
+  }
+  if (!process.env['IMAGE_PULL_SECRET']) {
+    process.env['IMAGE_PULL_SECRET'] = pull_secret_name(options);
+    print_if_debug(
+      options,
+      `'IMAGE_PULL_SECRET'=${process.env['IMAGE_PULL_SECRET']}`,
+    );
+  }
+  if (!process.env['INTERNAL_APP_PORT']) {
+    process.env['INTERNAL_APP_PORT'] = options.app_port;
+    print_if_debug(
+      options,
+      `'INTERNAL_APP_PORT'=${process.env['INTERNAL_APP_PORT']}`,
+    );
   }
 }
 
