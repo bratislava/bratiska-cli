c976225d2bcfe2f911a60e5898166959a4d6cbec
Richi <richard.dvorsky@bratislava.sk>
2024-06-27 11:10:59 +0200

    3.4.4 removed conditions for deployments (#76)

--- src/deploy/check_kubernetes_cluster_conditions.ts
+++ src/deploy/check_kubernetes_cluster_conditions.ts
@@ -41,62 +41,11 @@ export function check_kubernetes_cluster_conditions(options: Options) {
           "You cannot deploy to 'tkg-innov-prod' without a production flag! Please add the flag `--production` to the command.",
         );
       }
+
       if (options.untracked === true && options.force === false) {
         throw new Error(
           `You cannot deploy to 'tkg-innov-prod' when you have untracked changes. Please commit, and PR merge your changes to master!`,
         );
-      }
-      if (options.image) {
-        //turned off
-        if (
-          !helpers.is_master_image(options) &&
-          options.force === false &&
-          false
-        ) {
-          throw new Error(
-            `You cannot deploy to 'tkg-innov-prod' image which is not a master image! Please checkout the git branch to master, build master image, push to harbor and then you can use the master image.'`,
-          );
-        }
-
-        if (!helpers.is_deployment_image(options) && options.force === false) {
-          throw new Error(
-            `You cannot deploy to 'tkg-innov-prod' image which is not build from '${options.deployment}'!'`,
-          );
-        }
-      } else {
-        if (options.branch !== 'master' && options.force === false) {
-          throw new Error(
-            `You cannot deploy to 'tkg-innov-prod' when your current branch is not master. Please check out the git branch to master. Run 'git checkout master'`,
-          );
-        }
-        //turned off
-        if (options.merged === false && options.force === false && false) {
-          throw new Error(
-            `You cannot deploy to 'tkg-innov-prod' when the changes are not merged in the 'master' branch. Please create PR to propagate your changes to master!`,
-          );
-        }
-
-        if (options.gittag === false && options.force === false) {
-          throw new Error(
-            `You cannot deploy to 'tkg-innov-prod' when the changes are not tagged. Please tag and push your tags.!`,
-          );
-        }
-        //turned off
-        if (
-          options.gittag !== 'v' + options.version &&
-          false &&
-          options.force === false
-        ) {
-          throw new Error(
-            `Git tag version should match your package.json! Git tag: ${options.gittag} !== v${options.version}`,
-          );
-        }
-
-        if (options.origin_gittag === false && options.force === false) {
-          throw new Error(
-            `Push your local tag to origin because origin tag is not same as your local.`,
-          );
-        }
       }
       break;
   }
